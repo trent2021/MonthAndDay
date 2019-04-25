718dd40c4b7fabf6e64f7185e2d4407f2379ba54
Trent <trent.polenchar.2021@student.lhschools.org>
2019-04-25 13:12:19 -0400

    Changed If Statements to Cases

--- src/MonthAndDay.java
+++ src/MonthAndDay.java
@@ -43,7 +43,6 @@ public class MonthAndDay {
 
 
 
-
             if (day(m, d, Y) == 1) {
                 System.out.println("Your birthday is on a Sunday");
             } else if (day(m, d, Y) == 2) {
@@ -80,7 +79,6 @@ public class MonthAndDay {
             Y = Y - 1;
         }
 
-
         h = ((d + (13 * (m + 1) / 5) + Y + (Y / 4) - (Y / 100) + (Y / 400)) % 7);
 
         return h;
@@ -91,35 +89,50 @@ public class MonthAndDay {
         String answer;
         answer = "";
 
-
-        if (m == 1) {
-            answer = "There are 31 days in January";
-        } else if ((m == 2) && (d < 29)) {
-            answer = "There are 28 days in February";
-        } else if ((m == 2) && (d == 29)) {
-            answer = "There are 29 days in February";
-        } else if (m == 3) {
-            answer = "There are 31 days in March";
-        } else if (m == 4) {
-            answer = "There are 30 days in April";
-        } else if (m == 5) {
-            answer = "There are 31 days in May";
-        } else if (m == 6) {
-            answer = "There are 30 days in June";
-        } else if (m == 7) {
-            answer = "There are 31 days in July";
-        } else if (m == 8) {
-            answer = "There are 31 days in August";
-        } else if (m == 9) {
-            answer = "There are 30 days in September";
-        } else if (m == 10) {
-            answer = "There are 31 days in October";
-        } else if (m == 11) {
-            answer = "There are 30 days in November";
-        } else if (m == 12) {
-            answer = "There are 31 days in December";
+        switch (m) {
+            case 1:
+                answer = "There are 31 days in January";
+                break;
+            case 2:
+                if (d < 29) {
+                    answer = "There are 28 days in February";
+                    break;
+                }
+                if (d == 29) {
+                    answer = "There are 29 days in February";
+                    break;
+                }
+            case 3:
+                answer = "There are 31 days in March";
+                break;
+            case 4:
+                answer = "There are 30 days in April";
+                break;
+            case 5:
+                answer = "There are 31 days in May";
+                break;
+            case 6:
+                answer = "There are 30 days in June";
+                break;
+            case 7:
+                answer = "There are 31 days in July";
+                break;
+            case 8:
+                answer = "There are 31 days in August";
+                break;
+            case 9:
+                answer = "There are 30 days in September";
+                break;
+            case 10:
+                answer = "There are 31 days in October";
+                break;
+            case 11:
+                answer = "There are 30 days in November";
+                break;
+            case 12:
+                answer = "There are 31 days in December";
+                break;
         }
-
         return answer;
     }
 
@@ -127,24 +140,29 @@ public class MonthAndDay {
         String line;
         line = "";
 
-        if (h == 1) {
-            line = "You are fair and wise and good in every way";
-        } else if (h == 2) {
-            line = "You are fair of face";
-        } else if (h == 3) {
-            line = "You are full of grace";
-        } else if (h == 4) {
-            line = "You are full of woe";
-        } else if (h == 5) {
-            line = "You have far to go";
-        } else if (h == 6) {
-            line = "You are loving and giving";
-        } else if (h == 0) {
-            line = "You work hard for a living";
+        switch (h) {
+            case 1:
+                line = "You are fair and wise and good in every way";
+                break;
+            case 2:
+                line = "You are fair of face";
+                break;
+            case 3:
+                line = "You are full of grace";
+                break;
+            case 4:
+                line = "You are full of woe";
+                break;
+            case 5:
+                line = "You have far to go";
+                break;
+            case 6:
+                line = "You are loving and giving";
+                break;
+            case 0:
+                line = "You work hard for a living";
+                break;
         }
-
         return line;
-
-
     }
 }
